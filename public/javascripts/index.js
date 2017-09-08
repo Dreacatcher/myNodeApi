9987996135099873777ee560e7cd2c1f3d237e23
lucm <lucm@dcholdings.com>
2017-09-08 09:56:07 +0800

    s

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -8,7 +8,7 @@ $(function () {
     }
     $.ajax({
       type: 'POST',
-      url: 'http://localhost:3000/api/users/users0001',
+      url: 'http://120.25.197.246:3000/api/users/users0001',
       data: data,
       success: function () {
         console.log("dddd")
