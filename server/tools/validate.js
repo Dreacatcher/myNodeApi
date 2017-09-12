48e6ca6d33bcb3d3b24640ad6a5f0068717651c5
lucm <lucm@dcholdings.com>
2017-09-12 21:07:27 +0800

    增添

--- server/tools/validate.js
+++ server/tools/validate.js
@@ -2,15 +2,16 @@
  * @Author: lucm 
  * @Date: 2017-09-06 17:24:24 
  * @Last Modified by: lucm
- * @Last Modified time: 2017-09-12 17:53:33
+ * @Last Modified time: 2017-09-12 21:06:19
  */
 
 //生成MD5签名加密
 let crypto = require('crypto');
 let md5 = crypto.createHash('md5');
+var Base64 = require('js-base64').Base64;
 var Buffer = require('buffer').Buffer;
 // ******************************* Setting ***********************************/
-let appid = 'BASMD5-LCM-520-LYL'
+let appid = Base64.encode('BASMD5-LCM-520-LYL')
 let appkey = 'vws3236ce4fdscsfdsecdserr3232fdsf30d835243czxc4fds'
 let siteid = '520100'
 // ******************************* Setting ***********************************/
