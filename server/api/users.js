103a4d49eea881a688fd404de399f7009998bc34
lucm <lucm@dcholdings.com>
2017-09-14 11:08:47 +0800

    完成签名

--- server/api/users.js
+++ server/api/users.js
@@ -1,11 +1,9 @@
 let RegisteredUser = require('../module/registeredUser');
 let UsersInfo = require('../module/getUsersInfo');
-let crypto = require("crypto");
 var flash = require("connect-flash"); // 往session增加字段
 const validate = require('../../server/tools/validate');
 const common = require('../../server/tools/common');
 //生成MD5加密
-var md5 = crypto.createHash('md5');
 let userInfo = {
   /**
    * getUserInfo
@@ -16,10 +14,9 @@ let userInfo = {
   registeredUser(_param) {
     return function (cb) {
       let _userInfo = {}
-      let _password = md5.update(_param.password).digest('hex');
       let newUser = new RegisteredUser({})
       newUser.name = _param.name
-      newUser.password = _password
+      newUser.password = _param._password
       newUser.email = _param.email
       newUser.get(_param.name, function (err, user) {
         //用户已存在
@@ -97,6 +94,54 @@ let userInfo = {
         }
       })
     }
-  }
+  },
+  deleteUserInfos(_param) {
+    return function (cb) {
+      validate.validateHead(_param, function (_validate) {
+        let newUser = new UsersInfo({
+        })
+        let responseInfo = {}
+        if (_validate) {
+          newUser.getAllUsers(function (err, item) {
+            //用户已存在
+            if (err) {
+              responseInfo = {
+                code: 500,
+                datas: [],
+                status: err.message
+              }
+              _userInfo = common.responseInfo(responseInfo)
+              cb(null, _userInfo);
+            }
+            try {
+              responseInfo = {
+                code: 200,
+                datas: item,
+                status: '获取数据成功'
+              }
+              _userInfo = common.responseInfo(responseInfo)
+              cb(null, _userInfo);
+            } catch (e) {
+              responseInfo = {
+                code: 200,
+                datas: [],
+                status: e
+              }
+              _userInfo = common.responseInfo(responseInfo)
+              cb(null, _userInfo);
+            }
+          })
+        } else {
+          responseInfo = {
+            code: 200,
+            datas: [],
+            status: '验签失败'
+          }
+          _userInfo = common.responseInfo(responseInfo)
+          cb(null, _userInfo);
+        }
+      })
+    }
+  },
 }
 module.exports = userInfo;
---
+++ server/module/deleteUserInfos.js
@@ -0,0 +1,63 @@
+let mongodb = require("../db/usersDb.config");
+class getNewUserInfos {
+  constructor(name, password, email) {
+    this.name = name;
+    this.password = password;
+    this.email = email;
+  }
+  save(callback) {
+    //存储用户信息
+    let user = {
+      name: this.name,
+      password: this.password,
+      email: this.email
+    }
+    console.log("存储用户信息");
+    console.log(user)
+    //打开数据库
+    mongodb.open(function (err, db) {
+      if (err) {
+        return callback(err);
+      }
+      //读取registeruser集合
+      db.collection('usersInfo', function (err, collection) {
+        if (err) {
+          mongodb.close();
+          return callback(err);
+        }
+        //用户信息插入registeruser集合
+        collection.insert(user, function (err, user) {
+          mongodb.close();
+          if (err) {
+            return callback(err);
+          }
+          return callback(user.result.ok);
+        });
+      });
+    });
+  }
+  get(name, callback) {//读取用户信息
+    //打开数据库
+    mongodb.open(function (err, db) {
+      if (err) {
+        return callback(err);
+      }
+      db.collection('usersInfo', function (err, collection) {
+        if (err) {
+          callback(err);
+        }
+        //查找用户名
+        collection.findOne({
+          name: name
+        }, function (err, user) {
+          mongodb.close();
+          if (err) {
+            return callback(err);
+          }
+          callback(null, user);
+        });
+      });
+    });
+  };
+}
+module.exports = getNewUserInfos;
